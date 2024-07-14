b42ae994c25216940b205d2d8fa4062a94ae12f9
Minato235 <ankamasish@gmail.com>
2024-07-14 12:58:28 -0400

    npm init added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const head = React.createElement("h1", { id: "heading" }, "HHEl00000");
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-const parent = React.createElement("div", { id: "praent" }, [React.createElement("h4", { id: "child" }, "child")],
-    [React.createElement("h4", { id: "child" }, "child2")]);
+const parent = React.createElement("div", { id: "praent" },
+       [React.createElement("h4", { id: "child" }, "child")],
+       [React.createElement("h4", { id: "child" }, "child2")]);
 root.render(parent)
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-.
+# Episode1=>
+# Episode 2
