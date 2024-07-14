7253591603f8985fef81e0b5ec91f5db199817b0
Minato235 <ankamasish@gmail.com>
2024-07-14 12:00:12 -0400

    first class done

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-const head = React.createElement("h1", {}, "HHEl00000");
+const head = React.createElement("h1", { id: "heading" }, "HHEl00000");
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(head)
+
+const parent = React.createElement("div", { id: "praent" }, [React.createElement("h4", { id: "child" }, "child")],
+    [React.createElement("h4", { id: "child" }, "child2")]);
+root.render(parent)
