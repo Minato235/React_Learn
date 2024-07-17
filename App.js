1916f2352b806113cd872d7e9a6a981b54d6b3a7
Minato235 <ankamasish@gmail.com>
2024-07-17 11:03:48 -0400

    Day4

--- App.js
+++ App.js
@@ -1,20 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//react element
-const JsxHeading = () => <p id="heading">jsx Is ! html</p>;
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-//func componenet
-const FunctionComponeet = () => (
-    
-  <h3>
-    <JsxHeading />
-    {JsxHeading()}
-    Functional Component
-    s are Basic Js Functions
-  </h3>
-);
-root.render(<FunctionComponeet />);
-
-console.log(FunctionComponeet);
