5ec102cd1c8433a3156dbf5ab03ce223ae49278f
Minato235 <ankamasish@gmail.com>
2024-07-19 11:36:24 -0400

    Important

--- App.js
+++ App.js
@@ -1677,6 +1677,7 @@ const Body = () => {
       <div className="restroContainer">
         {resData.map((res) => {
           // console.log(res.info)
+          //data coming from down here   
           return <RestroCards key={res.info.id} {...res.info} />;
         })}
       </div>    
