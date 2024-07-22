f40c947f9986d096c2400cdaf9eca158433bd175
Minato235 <ankamasish@gmail.com>
2024-07-22 11:05:21 -0400

    day 5

---
+++ one.js
@@ -0,0 +1,15 @@
+var twoSum = function(nums, target) {
+
+    let map=new Map();
+    for(let i=0;i<nums.length;i++){
+        let x=target-nums[i]
+        if(map.has(x)){
+         return  [map.get(x),i]
+        }else{
+            map.set(nums[i],i)
+        }
+    }
+    return [-1,-1]
+};
+let nums = [2,7,11,15], target = 9;
+console.log(twoSum(nums,target))
